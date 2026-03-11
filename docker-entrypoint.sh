9d9469ac503168ea012c1f29da7e1dd20a27ad95
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2026-03-11 17:23:29 +0700

    fix runtime build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 if [ ! -d ".next" ]; then
   echo "Building Next.js application..."
   npm run build
+  cp -r .next/static .next/standalone/.next/static
 fi
 
 exec node .next/standalone/server.js
